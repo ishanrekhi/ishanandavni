7a8f3ca6f8b316e9e589259419a76f062242e366
ishanrekhi <ishanrekhi2007@gmail.com>
2026-06-12 23:20:25 -0500

    Add files via upload

---
+++ dates.js
@@ -0,0 +1,116 @@
+// ============================================================
+//  YOUR DATES — edit this file to add/change dates & photos
+// ============================================================
+
+const DATES = [
+  {
+    title: "First Date",
+    date: "Our First",
+    location: "Fairfax, VA",
+    lat: 38.8812581,
+    lng: -77.3913620,
+    story: "So, this is where it all began. I vividly remember our time walking around the house, my nervousness as we went for sushi, and how hard I was trying not to look like an idiot. Though, looking back, I'm so glad that all of this happened the way it did. None of what has become of us would have happened if it wasn't for this date. To this day, we still need to run a function back.",
+    photos: ["photos/pin1-firstdate.jpg"],
+  },
+  {
+    title: "Prom Night",
+    date: "Prom",
+    location: "Thomas Jefferson Memorial, Washington D.C.",
+    lat: 38.8816596,
+    lng: -77.0368160,
+    story: "To this day this has been one of my favorite dates that we've gotten on. Whether you consider it a date or not, it has been something that I will vividly remember forever. From waiting for an Uber to running in the rain, this night was highlighted by the time that you and I had together, and for that, I will always be grateful.",
+    photos: [
+      "photos/pin2-prom.jpg",
+    ],
+  },
+  {
+    title: "Will You Be My Girlfriend?",
+    date: "The Big Question",
+    location: "Centreville, VA",
+    lat: 38.8421557,
+    lng: -77.4358890,
+    story: "Still not over the fact that I never got an answer from you. As a serious reminder, please make sure that you answer whoever the next lucky guy you date. Anyways, this spot will forever mean a lot to me because we had a really nice initial date there, and then I got the honor of asking you to prom... and then the honor of making you my girlfriend at this very spot. I do think we should run back a date there at some point.",
+    photos: [
+      "photos/pin3-promposal.jpg",
+    ],
+  },
+  {
+    title: "Walks Around Town",
+    date: "Our Strolls",
+    location: "Vienna, VA",
+    lat: 38.9057430,
+    lng: -77.2598335,
+    story: "I truly miss all of our walks around town, whether in Vienna or Centreville. The ragebaiting, fun/deep talks, and opportunity to spend time with YOU is better than anything I could ask for. To the Vienna Giant that never has Boston Cream Donuts, we must run it again at some point.",
+    photos: [
+      "photos/pin4-walks.jpg",
+    ],
+  },
+  {
+    title: "First Rave",
+    date: "Echostage",
+    location: "Washington, D.C.",
+    lat: 38.9199037,
+    lng: -76.9724003,
+    story: "This night was truly a blast. From the odd pregame... to my weird dancing (that vid is never being released), it was so much fun bringing you to one of my raves. We must run back the trio once again.",
+    photos: [
+      "photos/pin5-rave.jpg",
+    ],
+  },
+  {
+    title: "My Crib",
+    date: "Home Base",
+    location: "Centreville, VA",
+    lat: 38.8467120,
+    lng: -77.4171905,
+    story: "This one pic doesn't capture all the memories we have here, but it'll suffice. From your awkward conversations with my parents to making pizza to TikTok scrolling time this place has been a great home base. Still need to take you out to dinner with my family then come back here after at some point though.",
+    photos: [
+      "photos/pin6-crib.jpg",
+    ],
+  },
+  {
+    title: "Ishan's Grad Party",
+    date: "Grad Party",
+    location: "Centreville, VA",
+    lat: 38.8463532,
+    lng: -77.4133276,
+    story: "Even though you left... :(, this was a blast. Super grateful that you showed up and spent the night with me after. :). From :( to :).",
+    photos: ["photos/pin7-gradparty.jpg"],
+  },
+  {
+    title: "Mall Adventure",
+    date: "Tysons",
+    location: "Tysons Corner Center, VA",
+    lat: 38.9191421,
+    lng: -77.2277024,
+    story: "Despite the crashout at the end, I had a super great time this day. We still have to go shopping once more and explore all the different zesty stores and take a mirror selfie in each one.",
+    photos: ["photos/pin8-mall.jpg"],
+  },
+  {
+    title: "Home Base",
+    date: "Always",
+    location: "Vienna, VA",
+    lat: 38.9137929,
+    lng: -77.2615542,
+    story: "It's been so nice to have this place (and movie room specifically) as our go-to spot. From the silly dinosaur and Madea movies to playing board games with Anika and alllll the conversations with your parents, I'll remember this place as being vital to us forever.",
+    photos: ["photos/pin10-homebase.jpg"],
+  },
+  {
+    title: "Graduation!",
+    date: "Graduation Day",
+    location: "EagleBank Arena, Fairfax, VA",
+    lat: 38.8268606,
+    lng: -77.3096753,
+    story: "Idk why I look so angry in this photo, but I'm so happy that we could be there at each other's graduations. It's been a blast and I'm so proud to see you finally make it through the hellhole that is TJ (jk). There's so much to come for both of us and I cannot wait to see what you do next :).",
+    photos: ["photos/pin9-graduation.jpg"],
+  },
+];
+
+// ============================================================
+//  SITE SETTINGS
+// ============================================================
+
+const SITE = {
+  title: "Ishan and Avni ♡",
+  subtitle: "Every Adventure.",
+  coupleNames: "Avni & Ishan",
+};
